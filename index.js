88a5965c1884d7066b72e9050231944cac3517ae
Nick Maliwacki <knic.knic@gmail.com>
2019-08-04 23:35:11 -0700

    switch to using node-fetch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var request = require('request');
+const fetch = require('node-fetch');
 var cheerio = require('cheerio');
 
 
@@ -6,19 +6,24 @@ var urls = []
 
 var startUrl = "https://www.webtoons.com/en/challenge/i-wish-i-were-you/list?title_no=117474&page=1"
 
-request(startUrl, function (err, resp, html) {
-    if (!err) {
-        let $ = cheerio.load(html);
-        let items = $('[data-episode-no]')
 
-        for (let i = 0; i != items.length; i++) {
-            let item = items[i]
-            if (item.attribs.href) {
-                console.log(item.attribs.href)
-                urls.push(item.attribs.href)
-            }
-        }
-        // console.log(html);
+const getData = async url => {
+    try {
+      const response = await fetch(url);
+      const html = await response.text();
+      const $ = cheerio.load(html);
+      let items = $('[data-episode-no]')
+
+      for (let i = 0; i != items.length; i++) {
+          let item = items[i]
+          if (item.attribs.href) {
+              console.log(item.attribs.href)
+              urls.push(item.attribs.href)
+          }
+      }
+    //   console.log(html);
+    } catch (error) {
+      console.log(error);
     }
-});
-
+  };
+  getData(startUrl);
